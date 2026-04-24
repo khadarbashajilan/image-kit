27100db54b0d439b216da80b809257f40ecf7f33
M KHADAR BASHA JILAN <khadarbashajilan001@gmail.com>
2026-04-24 16:36:14 +0530

    test: adding post to db and retrieving

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, File, Form, UploadFile
 from app.db import create_tables, get_db
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
+from sqlalchemy import select
 import logging
 from app.model import Post 
 
@@ -21,3 +22,39 @@ app = FastAPI(lifespan=lifespan)
 async def root():
     return {"message": "Hello World"}
 
+@app.post("/upload")
+async def upload_file(
+    file: UploadFile = File(...),
+    caption: str = Form(""),
+    session:AsyncSession = Depends(get_db)    
+):
+    post = Post(
+        caption=caption,
+        url="dummy url",
+        file_type = "photo",
+        file_name = "dummy name"
+    )
+    
+    session.add(post)
+    await session.commit()
+    await session.refresh(post)
+    return post
+    
+@app.get("/feed")
+async def feed(session:AsyncSession = Depends(get_db)):
+    result = await session.execute(select(Post).order_by(Post.created_at.desc()))
+    posts = [rows[0] for rows in result.all()]
+    
+    posts_data = []
+    for post in posts:
+        posts_data.append(
+            {
+                "id":str(post.id),
+                "caption":post.caption,
+                "url":post.url,
+                "file_type":post.file_type,
+                "file_name": post.file_name,
+                "created_at": post.created_at.isoformat()
+            }
+        )
+    return {"posts": posts_data}
